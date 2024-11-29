8a3e2156ea5bc82fe7acc5320dd34be17c72a229
Ahmed Raouf <ahmedraouf.metwally@gmail.com>
2024-11-29 10:31:46 +0200

    deployment to railway

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,23 +1,27 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const { UnauthenticatedError } = require('../errors');
+const User = require( '../models/User' );
+const jwt = require( 'jsonwebtoken' );
+const { UnauthenticatedError } = require( '../errors' );
 
-const auth = async (req, res, next) => {
+const auth = async ( req, res, next ) =>
+{
   // check header
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Authentication invalid');
+  if ( !authHeader || !authHeader.startsWith( 'Bearer' ) )
+  {
+    throw new UnauthenticatedError( 'Authentication invalid' );
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split( ' ' )[ 1 ];
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+  try
+  {
+    const payload = jwt.verify( token, process.env.JWT_SECRET );
     // attach the user to the job routes
-    const testUser = payload.userId === '62f801d0510a7c1ed2312d52';
+    const testUser = payload.userId === '67493646416a5a90ff0b732f';
     req.user = { userId: payload.userId, testUser };
     next();
-  } catch (error) {
-    throw new UnauthenticatedError('Authentication invalid');
+  } catch ( error )
+  {
+    throw new UnauthenticatedError( 'Authentication invalid' );
   }
 };
 
